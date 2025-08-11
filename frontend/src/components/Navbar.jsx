1c41e612bac198df1ee13d2e3227bb56fcba640a
sujeet kumar <syadav447@rku.ac.in>
2025-08-11 11:01:01 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary" />
               </div>
-              <h1 className="text-lg font-bold hidden sm:block">YaduvanshiChattingApp</h1>
+              <h1 className="text-lg font-bold hidden sm:block">SKYChattingApp</h1>
               <h1 className="text-lg font-bold sm:hidden">ChatApp</h1>
             </Link>
           </div>
